d38350f5c5fc20f8ae01f0d96acb1a1de67a52a4
Matthew Pengelly <pengelly.mat@gmail.com>
2016-02-24 20:17:49 -0500

    refactor character reading and argument reading into seperate functions

--- convert-case.js
+++ convert-case.js
@@ -1,31 +1,57 @@
 // read the first command line argument
-var arg = process.argv[2];
-var word = '';
-var curr_char = 0;
-var curr_arg = 2; // skip first two indicies
-var field = '';
-
 var _ = require('lodash');
+var index_offset = 2;
+var argIndex = 0 + index_offset; // skip first two indicies
+// var fieldList = [];
 
 // todo refactor
 // reads the current strings characters and formats the output to snake case
-function readCharacters (callback) {
-  if (!arg) throw Error('input argument is undefined');
+function readCharacters (word) {
+  var result = '';
+  var charIndex = 0;
+  // cache length
+  var wordLength = word.length - 1;
+  while (charIndex <= wordLength) {
+    result += word[charIndex];
+    charIndex += 1;
+  }
+  return result;
+}
+
+function nextArgument (callback) {
+  if (!process.argv) throw Error('input argument is undefined');
+
+  var word = '';
+  var field = '';
 
   // cache length
   var argsLength = process.argv.length - 1;
-  var wordLength = arg.length - 1;
-  while (curr_arg <= argsLength) {
-    while (curr_char <= wordLength) {
-      word += arg[curr_char];
-      curr_char += 1;
-    }
-    curr_char = 0; // reset current character index
-    curr_arg += 1; // increment argument index
-    field += word; // concat field with new word
+  while (argIndex <= argsLength) {
+    word = process.argv[argIndex];
+    field += readCharacters(word);
+    argIndex += 1;
   }
-
   callback(field);
 }
 
-readCharacters(console.log);
+nextArgument(console.log);
+
+// // todo refactor
+// // reads the current strings characters and formats the output to snake case
+// function readCharacters (word) {
+//   // cache length
+//   var argsLength = process.argv.length - 1;
+
+//   while (argIndex <= argsLength) {
+//     word = process.argv[argIndex];
+//     var wordLength = word.length - 1;
+//     while (charIndex <= wordLength) {
+//       result += word[charIndex];
+//       charIndex += 1;
+//     }
+//     field += result; // concat field with new word
+//     result = ''; // reset result
+//     charIndex = 0; // reset current character index
+//     argIndex += 1; // increment argument index
+//   }
+// }
